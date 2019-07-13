d47e6fe6a52aea52127504b093e9fea1b1553c23
katsa9 <kathleen.brand2@gmail.com>
2019-07-13 12:46:30 +0200

    feat: add middleware and connect App component

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,10 +1,11 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
-import { setAuthedUser } from '../actions/tweets'
+import { setAuthedUser } from '../actions/authedUser'
 
 const AUTHED_USER_ID = 'sarah_edo'
 
+//thunk action creator as it returns a function
 export function handleInitialData() {
   return (dispatch) => {
     return getInitialData()
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(handleInitialData()) //we have access to dispatch because when we connect this component to the store it gets the mapped slice of state (none in this case) as well as dispatch as a prop
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +17,6 @@ class App extends Component {
   }
 }
 
-export default App
+//connect the App component to the store
+//Using the connect() function upgrades a component to a container. Containers can read state from the store and dispatch actions.
+export default connect()(App)
